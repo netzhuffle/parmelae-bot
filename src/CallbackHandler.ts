c465cb3dc6b5094e50055100fed61c8bdb07fb5d
Jannis Grimm <j@jannis.rocks>
2023-06-03 19:16:24 +0000

    Add promise to get full generated text
    Agents return only the text to send. For prompts and history, the actual
    full text including thoughts is necessary to stay on track.
    
    This enables the storage of full text in a later change.
    
    no_announcement

--- src/CallbackHandler.ts
+++ src/CallbackHandler.ts
@@ -1,17 +1,26 @@
 import { BaseCallbackHandler } from 'langchain/callbacks';
 import { TelegramService } from './TelegramService';
-import { AgentAction } from 'langchain/schema';
+import { AgentAction, ChainValues, LLMResult } from 'langchain/schema';
 import { IntermediateAnswerTool } from './Tools/IntermediateAnswerTool';
+import { assert } from 'console';
 
 /** Handles LangChain callbacks. */
 export class CallbackHandler extends BaseCallbackHandler {
   name = 'CallbackHandler';
+  readonly fullTextPromise: Promise<string>;
+  private resolveFullText: (value: string) => void;
 
   constructor(
     private readonly telegram: TelegramService,
     private readonly chatId: bigint,
   ) {
     super();
+    this.resolveFullText = () => {
+      // Overridden below.
+    };
+    this.fullTextPromise = new Promise((resolve) => {
+      this.resolveFullText = resolve;
+    });
   }
 
   async handleAgentAction(action: AgentAction): Promise<void> {
@@ -22,5 +31,12 @@ export class CallbackHandler extends BaseCallbackHandler {
     return this.telegram
       .sendWithoutStoring(`[${action.tool}: ${action.toolInput}]`, this.chatId)
       .then();
+  }
+
+  handleChainEnd(outputs: ChainValues): void {
+    if (outputs.text) {
+      assert(typeof outputs.text === 'string');
+      this.resolveFullText(outputs.text as string);
+    }
   }
 }
